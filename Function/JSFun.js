9052c9c80007b046e304dc180cad9c5ebe44ec5f
push <anoopsingh145g@gmail.com>
2025-11-02 03:08:32 +0000

    commit

--- Function/JSFun.js
+++ Function/JSFun.js
@@ -44,3 +44,31 @@ let result = add(5, 7);
 console.log(result);   // 12
 
 
+function multiply(x,y) {
+    return x * y;
+}
+let product = multiply(4,6);
+console.log(product);
+
+
+function sayname(name){
+    console.log("My name is " + name);
+}
+sayname();  //undefined
+sayname('Anup');
+
+function addition(number1, number2){
+    console.log(number1 + number2);
+}
+addition(10,20);
+addition(5,"a"); // 5a
+const result1 = addition(3,4);
+console.log(result1);  //undefined because addition function does not return any value
+
+function divide(num1,num2){
+    let division = num1 / num2;
+    return division
+}
+const division = divide(20,4);
+console.log(division);
+
